020cbd2965863f11d648faebd3acd0566fa83883
gviswanathan2594 <gviswanathan@sdimktg.com>
2019-07-23 17:02:51 -0400

    about and contact html updated

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -1,7 +1,109 @@
 import React from "react";
 
 function About() {
-    return (<h1 className="mt-5">About Page</h1>);
+    return (
+        <div className="container">
+            <h1 className="page-heading">About Page</h1>
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <div className="card shadow-none fade-in">
+                        <div className="media">
+                            <div className="media-body">
+                                <h4 className="mt-0 mb-1">Gokul Viswanathah</h4>
+                                <p>
+                                    <span className="bold">Passion: </span>
+                                    <span className="badge badge-dark">DataScience Enthusist</span>
+                                    <span className="badge badge-dark">Open source</span>
+                                    <span className="badge badge-dark">Programming</span>
+                                    <span className="badge badge-dark">Soccer</span>
+                                    <span className="badge badge-dark">Gaming</span>
+                                </p>
+
+                                <div className="mt-3">
+                                    <h5><u>Education</u></h5>
+                                    <div>
+                                        <h6>Georgian College</h6>
+                                        <span>Computer Programmer [2017-2019]</span>
+                                    </div>
+                                    <br/>
+                                    <div>
+                                        <h6>Anna University</h6>
+                                        <span>Electronics and Communication Engineering [2012-2016]</span>
+                                    </div>
+                                </div>
+
+                                <div className="mt-3">
+                                    <h5><u>Skills:</u></h5>
+                                    <p>
+                                        <span className="badge badge-dark">JavaScript</span>
+                                        <span className="badge badge-dark">Java</span>
+                                        <span className="badge badge-dark">C#</span>
+                                        <span className="badge badge-dark">Python</span>
+                                        <span className="badge badge-dark">NodeJS</span>
+                                        <span className="badge badge-dark">ReactJS</span>
+                                        <span className="badge badge-dark">Grails</span>
+                                        <span className="badge badge-dark">Asp.net</span>
+                                        <span className="badge badge-dark">Spring boot</span>
+                                        <span className="badge badge-dark">GIT</span>
+                                        <span className="badge badge-dark">Heroku</span>
+                                        <span className="badge badge-dark">Linux</span>
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <div className="card shadow-none fade-in-2">
+                        <div className="media">
+                            <div className="media-body">
+                                <h4 className="mt-0 mb-1">Mathew D'Agustino</h4>
+                                <p>
+                                    <span className="bold">Passion: </span>
+                                    <span className="badge badge-dark">Programmer</span>
+                                    <span className="badge badge-dark">Basketball</span>
+                                    <span className="badge badge-dark">Online Gaming</span>
+                                </p>
+
+                                {/* Education section */}
+                                <section>
+                                    <div class="mt-3">
+                                        <h5><u>Education</u></h5>
+                                        <div className="">
+                                            <h6>Georgian College</h6>
+                                            <span>Computer Programmer [2017-2019]</span>
+                                        </div>
+                                        <br/>
+                                    </div>
+                                </section>
+                                {/* Skills section */}
+                                <section>
+                                    <div className="mt-3">
+                                        <h5><u>Skills:</u></h5>
+                                        <p>
+
+                                            <span className="badge badge-dark">Java</span>
+                                            <span className="badge badge-dark">JavaScript</span>
+                                            <span className="badge badge-dark">NodeJS</span>
+                                            <span className="badge badge-dark">ReactJS</span>
+                                            <span className="badge badge-dark">Grails</span>
+                                            <span className="badge badge-dark">Asp.net</span>
+                                            <span className="badge badge-dark">Spring boot</span>
+                                            <span className="badge badge-dark">GIT</span>
+                                            <span className="badge badge-dark">Heroku</span>
+                                            <span className="badge badge-dark">Linux</span>
+                                        </p>
+                                    </div>
+                                </section>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default About;
--- client/src/components/pages/contact.js
+++ client/src/components/pages/contact.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 function Contact() {
-    return (<h1 className="mt-5">Contact Page</h1>);
+    return (
+        <div className="container">
+            <h1 className="page-heading">Contact Page</h1>
+            <div className="row">
+                <div className="card contact-card fade-in">
+                    <div className="card-body text-center">
+                        <h4>Gokul Viswanathan</h4>
+                        <h6 className="card-title">Software Developer</h6>
+                        <span className="bold">Email:</span>
+                        <span className="bold">gokul@hogwartz.com</span><br />
+                        <span className="bold">Owls: </span>
+                        <span className="bold">Hedwig </span>
+                    </div>
+                </div>
+
+                <div className="card contact-card">
+                    <div className="card-body text-center fade-in-2">
+                        <h4>Matthew D Augustino</h4>
+                        <h6 className="card-title">Software Developer</h6>
+                        <span className="bold">Email:</span>
+                        <span className="bold">matthew@hogwartz.com</span><br />
+                        <span className="bold">Owls: </span>
+                        <span className="bold">Pigwidgeon </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
 }
 
 export default Contact;
